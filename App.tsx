a98539ff91aaa1d52cb5894c5771e2fcc0a68c7e
jaanisfehling <jaanisfehling@gmail.com>
2023-01-15 01:36:20 +0100

    setup headline screen basic styling

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {SafeAreaProvider} from "react-native-safe-area-context";
-import {SafeAreaView} from "react-native";
 import ArticleScreen from "./screens/ArticleScreen";
 import HeadlineScreen from "./screens/HeadlineScreen";
 
@@ -11,15 +9,12 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
-        <SafeAreaView>
-            <SafeAreaProvider>
-                <NavigationContainer>
-                    <Stack.Navigator initialRouteName="Home">
-                        <Stack.Screen name="Headlines" component={HeadlineScreen}/>
-                        <Stack.Screen name="Article" component={ArticleScreen}/>
-                    </Stack.Navigator>
-                </NavigationContainer>
-            </SafeAreaProvider>
-        </SafeAreaView>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Headlines"
+                             screenOptions={{headerShown: false}}>
+                <Stack.Screen name="Headlines" component={HeadlineScreen}/>
+                <Stack.Screen name="Article" component={ArticleScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
--- screens/HeadlineScreen.js
+++ screens/HeadlineScreen.js
@@ -1,29 +1,56 @@
 import React from 'react';
-import {FlatList, View} from 'react-native';
+import {FlatList, View, StyleSheet} from 'react-native';
+import {Card, Text} from 'react-native-paper';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 22,
+        backgroundColor: '#000000',
     },
-    item: {
-        padding: 10,
+    card: {
+        backgroundColor: "#002a5d",
+        borderRadius: 18,
+        margin: 5,
+    },
+    title: {},
+    headline: {
         fontSize: 18,
-        height: 44,
+        fontWeight: "bold",
+        color: "white",
+    },
+    date: {
+        fontSize: 10,
+        color: "white",
+    },
+    summary: {
+        fontSize: 12,
+        color: "white",
     },
 });
 
-export default function HeadlineScreen() {
+export default function HeadlineScreen({navigation}) {
     return (
-        <View>
+        <View style={styles.container}>
             <FlatList
                 data={[
-                    {key: "Headline 1"},
-                    {key: "Headline 2"},
-                    {key: "Headline 3"},
+                    {headline: "Headline", date: "January 13, 2023 06:55", summary: "This is a major Change"},
+                    {headline: "Headline", date: "January 13, 2023 06:54", summary: "Breaking News"},
+                    {
+                        headline: "Headline",
+                        date: "January 13, 2023 06:53",
+                        summary: "The World is dying and we should be worried. This text is stupid fuckk off"
+                    },
                 ]}
-                renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-            />
+                renderItem={({item}) =>
+                    <Card style={styles.card} onPress={() => navigation.navigate("Article")}>
+                        <Card.Title style={styles.title} titleStyle={styles.headline} subtitleStyle={styles.date}
+                                    title={item.headline}
+                                    subtitle={item.date}/>
+                        <Card.Content>
+                            <Text variant="bodyMedium" style={styles.summary}>{item.summary}</Text>
+                        </Card.Content>
+                    </Card>
+                }/>
         </View>
     );
 }
