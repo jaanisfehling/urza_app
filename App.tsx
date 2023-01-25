d5744ab8b8a03eaa8a15ba90d2ff6a81a9a85f72
jaanisfehling <jaanisfehling@gmail.com>
2023-01-25 19:33:52 +0100

    add route to trading screen

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import ArticleScreen from "./screens/ArticleScreen";
 import HeadlineScreen from "./screens/HeadlineScreen";
+import {Button} from "react-native";
 
 
 const Stack = createNativeStackNavigator();
@@ -12,7 +13,15 @@ export default function App() {
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Headlines">
                 <Stack.Screen name="Headlines" component={HeadlineScreen}/>
-                <Stack.Screen name="Article" component={ArticleScreen}/>
+                <Stack.Screen name="Article" component={ArticleScreen} options={({navigation, route}) => ({
+                    headerRight: () => (
+                        <Button
+                            onPress={() => navigation.navigate("Trading", route.params.tickers)}
+                            title="Trade"
+                            color="#123456"
+                        />)
+                })}/>
+                <Stack.Screen name="Trading" component={TradingScreen}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
